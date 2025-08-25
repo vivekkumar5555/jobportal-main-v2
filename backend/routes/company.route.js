ed86bffac51f14fa30c94e984da21215e20e891d
vivek kumar <vivekkumar60403@gmail.com>
2025-08-25 20:26:04 +0530

    initial commit

---
+++ backend/routes/company.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/mutler.js";
+
+const router = express.Router();
+
+router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
+router.route("/get/:id").get(isAuthenticated,getCompanyById);
+router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
+
+
+// router.route("/register",registerCompany);
+// router.route("/get",getCompany);
+// router.route("/get/:id",getCompanyById);
+// router.route("/update/:id",singleUpload, updateCompany);
+
+export default router;
+
